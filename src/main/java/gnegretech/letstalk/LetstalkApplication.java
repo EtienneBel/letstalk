3f6a1b0cb56c4e8618171f559338f0cd1a4baef1
Etienne Wend-Kuuni Belemgnegre <ebelemgnegre@nine7soft.com>
2023-09-21 21:48:20 +0000

    re-initial commit

---
+++ src/main/java/gnegretech/letstalk/LetstalkApplication.java
@@ -0,0 +1,13 @@
+package gnegretech.letstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetstalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetstalkApplication.class, args);
+	}
+
+}
